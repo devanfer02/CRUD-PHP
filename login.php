8a73e5136f14bbfa14fa39174972eb6cbed2a748
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2023-04-11 12:17:25 +0700

    update login page

--- login.php
+++ login.php
@@ -13,45 +13,83 @@ if(isset($_POST["login"]))
 <html lang="en" data-bs-theme="dark">
 <?php include 'addons/head.php';?>
 <body>
-    <!-- <?php include 'addons/navbar.php';?> -->
-    <div class="d-flex justify-content-center align-items-center border-bottom mb-4" style="padding-top: 10px;">
-        <h1 class="text-center" style="padding-bottom: 10px;">
-            [Login Page]
-        </h1>
-    </div>
-    <form action="" method="post" style="padding-top: 10px; padding-left: 14px">
-        <div class="mb-3">
-            <label for="username" class="form-label">Username</label>
-            <input type="text" class="form-control" name="username" id="username" autocomplete="off" required>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="text-center">Login</h3>
+                </div>
+                <div class="card-body">
+                    <form action="" method="post" >
+                        <div class="form-group">
+                            <label for="username" class="label">Username</label>
+                            <input type="text" class="form-control" id="username" name="username"
+                            placeholder="Enter username" autocomplete="off" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password"
+                            placeholder="Enter password" autocomplete="off" required>
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" name="remember" id="remember">
+                            <label class="form-check-label" for="remember">
+                                Remember Me
+                            </label>
+                        </div>
+                        <div>
+                            <?php if(isset($status) && $status ): ?>
+                                <?php header("Location: index.php"); exit;?>
+                            <?php elseif(isset($status)) : ?>
+                                <p class="warn">
+                                    Username or Password doesn't match
+                                </p>
+                            <?php endif;?>
+                        </div>
+                        <div class="text-center login-btn">
+                            <button type="submit" class="btn btn-primary" name="login">Login</button>
+                        </div>
+                    </form>
+                </div>
+                <hr>
+                <div class="card-body register">
+                    <h5>
+                        Don't have an account yet? 
+                    </h5>
+                    <a href="register.php" class="btn btn-primary" style="padding-bottom: 8px;">Register</a>
+                </div>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" name="password" id="password" autocomplete="off" required>
-        </div>
-        <div class="form-check" style="padding-bottom:8px;">
-            <input type="checkbox" class="form-check-input" name="remember" id="remember">
-            <label class="form-check-label" for="remember">
-                Remember Me
-            </label>
-        </div>
-        <button type="submit" class="btn btn-primary" name="login">Login</button>
-    </form>
-    <div>
-        <?php if(isset($status) && $status ): ?>
-            <?php header("Location: index.php"); exit;?>
-        <?php elseif(isset($status)) : ?>
-            <p style="color:red; padding-left:15px; padding-top:7px;">
-                Username or Password doesn't match
-            </p>
-        <?php endif;?>
-    </div>
-    <div style="padding-left:15px; padding-top:7px;">
-        <hr>
-        <h4>
-            Don't have an account yet? 
-        </h4>
-        <a href="register.php" class="btn btn-primary" style="padding-bottom: 8px;">Register</a>
-        <hr>
     </div>
 </body>
+<style>
+    .login-btn{
+        margin-top:20px;
+    }
+    .text-center{
+        padding-top: 10px;
+    }
+    .label{
+        padding-top:10px;
+        padding-bottom: 5px;
+    }
+    .btn{
+        margin-top:5px;
+    }
+    .register{
+        margin-left: 1px;
+    }
+    .form-check{
+        margin-top:12px;
+        margin-left:1px;
+    }
+    .warn{
+        color: red;
+    }
+
+    form{
+        min-height: 220px;
+    }
+</style>
 </html>
